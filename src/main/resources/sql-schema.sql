9e9576b0e8f90d71aea2708cced7fa03cab58bf0
Md Joni <mjoni@qa.com>
2020-09-11 15:53:19 +0100

    change db property files

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,7 +1,7 @@
-drop schema ims;
-CREATE SCHEMA IF NOT EXISTS `ims`;
-USE `ims` ;
-CREATE TABLE IF NOT EXISTS `ims`.`customers` (
+drop schema user_inventory_system;
+CREATE SCHEMA IF NOT EXISTS `user_inventory_system`;
+USE `user_inventory_system` ;
+CREATE TABLE IF NOT EXISTS `user_inventory_system`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) NULL DEFAULT NULL,
     `surname` VARCHAR(40) NULL DEFAULT NULL,
