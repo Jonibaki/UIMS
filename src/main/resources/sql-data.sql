ed896b64ff643da3f0372ea01e3ee1417f0cf199
Md Joni <mjoni@qa.com>
2020-09-12 23:39:32 +0100

    Update sql template file

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,1 @@
-INSERT INTO `user_inventory_system`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `user_inventory_system`.`customers` (`first_name`, `surname`) VALUES ('Joni', 'Baki');
