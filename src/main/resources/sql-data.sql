1506324d3396c9473451aea984707c223f79398b
Md Joni <mjoni@qa.com>
2020-09-14 12:24:56 +0100

    update sql relational table with dummy data

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,5 @@
 INSERT INTO `user_inventory_system`.`customers` (`first_name`, `surname`) VALUES ('Joni', 'Baki');
+
+
+INSERT INTO `user_inventory_system`.`products` (`product_name`, `category`,`price`) VALUES ('Pepsi', 'Drinks', 2.0);
+
