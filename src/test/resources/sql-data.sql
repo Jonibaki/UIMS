cdcde3d243a27c7f030156c738269da51acf78c1
Md Joni <mjoni@qa.com>
2020-09-16 22:01:38 +0100

    Add a real database data to test all the classes

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,7 @@
-INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `user_inventory_system`.`customers` (`first_name`, `surname`) VALUES ('Joni', 'Baki');
+
+INSERT INTO `user_inventory_system`.`products` (`product_name`, `category`,`price`) VALUES ('Pepsi', 'Drink', 2.5);
+
+INSERT INTO `user_inventory_system`.`orders` (`customerId`) VALUES (1);
+
+INSERT INTO `user_inventory_system`.`orderItems` (`orderId`, `pId`, `quantity`) VALUES (1,2,10);
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,7 +1,31 @@
-DROP TABLE IF EXISTS `customers`;
-CREATE TABLE IF NOT EXISTS `customers` (
+drop schema user_inventory_system;
+CREATE SCHEMA IF NOT EXISTS `user_inventory_system`;
+USE `user_inventory_system` ;
+
+CREATE TABLE IF NOT EXISTS `user_inventory_system`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) NULL DEFAULT NULL,
     `surname` VARCHAR(40) NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+CREATE TABLE IF NOT EXISTS `user_inventory_system`.`products` (
+    `pId` INT(11) NOT NULL AUTO_INCREMENT,
+    `product_name` VARCHAR(40) NULL DEFAULT NULL,
+    `category` VARCHAR(40) NULL DEFAULT NULL,
+    `price` DOUBLE NULL DEFAULT NULL,
+    PRIMARY KEY (`pId`)
+);
+CREATE TABLE IF NOT EXISTS `user_inventory_system`.`orders` (
+    `orderId` INT(11) NOT NULL AUTO_INCREMENT,
+    `customerId` INT(11) NOT NULL,
+    PRIMARY KEY (`orderId`),
+    foreign key (`customerId`) references customers (`id`)
+);
+CREATE TABLE IF NOT EXISTS `user_inventory_system`.`orderItems` (
+    `orderId` INT(11) NOT NULL,
+    `pId` INT(11) NOT NULL,
+    `quantity` INT(11),
+    PRIMARY KEY (`orderId`, pId),
+    CONSTRAINT foreign key (`orderId`) references orders (`orderId`) ON DELETE CASCADE,
+    CONSTRAINT foreign key (`pId`) references products (`pId`) ON DELETE CASCADE
 );
