0700059a1b16491c52e10fe3b4ee223c971004e9
Md Joni <mjoni@qa.com>
2020-09-16 13:18:41 +0100

    Add more data into sql data file and change schema

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -8,3 +8,18 @@ INSERT INTO `user_inventory_system`.`customers` (`first_name`, `surname`) VALUES
 INSERT INTO `user_inventory_system`.`products` (`product_name`, `category`,`price`) VALUES ('Pepsi', 'Drinks', 2.0);
 INSERT INTO `user_inventory_system`.`products` (`product_name`, `category`,`price`) VALUES ('Coke', 'Drinks', 5.0);
 INSERT INTO `user_inventory_system`.`products` (`product_name`, `category`,`price`) VALUES ('Laptop', 'Tech', 1000.0);
+
+INSERT INTO `user_inventory_system`.`orders` (`customerId`) VALUES (1);
+INSERT INTO `user_inventory_system`.`orders` (`customerId`) VALUES (2);
+INSERT INTO `user_inventory_system`.`orders` (`customerId`) VALUES (3);
+
+
+INSERT INTO `user_inventory_system`.`orderItems` (`orderId`, `pId`, `quantity`) VALUES (1,2,10);
+INSERT INTO `user_inventory_system`.`orderItems` (`orderId`, `pId`, `quantity`) VALUES (1,3,5);
+INSERT INTO `user_inventory_system`.`orderItems` (`orderId`, `pId`, `quantity`) VALUES (2,3,1);
+INSERT INTO `user_inventory_system`.`orderItems` (`orderId`, `pId`, `quantity`) VALUES (3,3,2);
+INSERT INTO `user_inventory_system`.`orderItems` (`orderId`, `pId`, `quantity`) VALUES (3,2,2);
+
+
+
+
