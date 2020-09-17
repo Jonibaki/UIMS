56c1d8fee0b1324c4b08a4248d1b6dd6b669eab2
Md Joni <mjoni@qa.com>
2020-09-17 12:13:10 +0100

    Make some changes in sql schema which handle deletion of customer

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -19,13 +19,13 @@ CREATE TABLE IF NOT EXISTS `user_inventory_system`.`orders` (
     `orderId` INT(11) NOT NULL AUTO_INCREMENT,
     `customerId` INT(11) NOT NULL,
     PRIMARY KEY (`orderId`),
-    foreign key (`customerId`) references customers (`id`)
+    CONSTRAINT foreign key (`customerId`) references customers (`id`) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS `user_inventory_system`.`orderItems` (
     `orderId` INT(11) NOT NULL,
     `pId` INT(11) NOT NULL,
     `quantity` INT(11),
-    PRIMARY KEY (`orderId`, pId),
+    PRIMARY KEY (`orderId`, `pId`),
     CONSTRAINT foreign key (`orderId`) references orders (`orderId`) ON DELETE CASCADE,
     CONSTRAINT foreign key (`pId`) references products (`pId`) ON DELETE CASCADE
 );
